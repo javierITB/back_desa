a4570936c6e4f4727249c8ccfa3bf0b82ce6cdc0
javierITB <vicente.torresb@mail.udp.cl>
2026-01-27 09:47:33 -0300

    registros

--- endpoints/auth.js
+++ endpoints/auth.js
@@ -1567,7 +1567,16 @@ router.get("/empresas/usuarios/:email", async (req, res) => {
    }
 });
 
-
+router.get("/registros/todos", async (req, res) => {
+   try {
+      await verifyRequest(req);
+      const tkn = await req.db.collection("cambios").find().toArray();
+      res.json(tkn);
+   } catch (err) {
+      if (err.status) return res.status(err.status).json({ message: err.message });
+      res.status(500).json({ error: "Error al obtener registros" });
+   }
+});
 
 
 router.get("/mantenimiento/migrar-empresas-pqc", async (req, res) => {
